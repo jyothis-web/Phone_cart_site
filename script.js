8b480f28545f2eb7c3c8754f79519f0c326b45d8
JYOTHISWARUP E V <jyothiswarup99@gmail.com>
2023-09-22 17:12:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+var menuicon = document.getElementById("menu-icon");
+var sidebar = document.getElementById("sidebar");
+menuicon.addEventListener('click',()=>{
+    sidebar.classList.toggle("small-sidebar")});
